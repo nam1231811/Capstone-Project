7927f2bb0b215089b286019b7f25f4dedb3c25af
Vũ Nhật Nam <namvnse181565@fpt.edu.vn>
2026-03-16 16:37:59 +0000

    Fix lỗi filter không load được data + pagination cho data table

--- webapp/utils/FilterData.js
+++ webapp/utils/FilterData.js
@@ -15,20 +15,23 @@ sap.ui.define([
             }
             
             this._oFilterDialog.removeAllFilterItems();
-            var aData = this.getView().getModel("displayModel").getProperty("/Data");
+            
+            //Lấy dữ liệu từ /UiData
+            var aData = this.getView().getModel("displayModel").getProperty("/UiData");
             
             this._oFieldName.forEach(function (sFieldName, index) {
                 var oFilterItem = new sap.m.ViewSettingsFilterItem({
-                    key: index,
+                    key: index.toString(), //Đảm bảo key là chuỗi
                     text: sFieldName
                 });
                 
                 var aUniqueValues = [];
                 if (aData) {
                     aData.forEach(function(aRow) {
-                        if (aRow[index] && aRow[index].value) {
-                            var sValue = aRow[index].value.toString();
-                            if (aUniqueValues.indexOf(sValue) === -1) {
+                        //Đảm bảo cell tồn tại và có giá trị
+                        if (aRow[index] && aRow[index].value !== undefined && aRow[index].value !== null) {
+                            var sValue = aRow[index].value.toString().trim();
+                            if (aUniqueValues.indexOf(sValue) === -1 && sValue !== "") {
                                 aUniqueValues.push(sValue);
                             }
                         }
@@ -42,7 +45,10 @@ sap.ui.define([
                     }));
                 });
 
-                this._oFilterDialog.addFilterItem(oFilterItem);
+                //Chỉ hiện những cột nào thực sự có dữ liệu
+                if (aUniqueValues.length > 0) {
+                    this._oFilterDialog.addFilterItem(oFilterItem);
+                }
             }.bind(this));
 
             this._oFilterDialog.open();
@@ -54,34 +60,63 @@ sap.ui.define([
                 oBinding = oTable.getBinding("rows");
 
             var aSelectedItems = mParams.filterItems;
+            
+            //Thực hiện filter
             if (aSelectedItems.length === 0) {
                 oBinding.filter([]);
-                return;
+            } else {
+                var oFilterGroups = {};
+                aSelectedItems.forEach(function(oItem) {
+                    var aSplit = oItem.getKey().split("___");
+                    var sColIndex = aSplit[0];
+                    var sValue = aSplit[1];
+                    
+                    if (!oFilterGroups[sColIndex]) {
+                        oFilterGroups[sColIndex] = [];
+                    }
+                    oFilterGroups[sColIndex].push(sValue.trim());
+                });
+
+                var oCustomFilter = new sap.ui.model.Filter({
+                    path: "", 
+                    test: function(aRow) {
+                        if (!aRow || !Array.isArray(aRow)) return false;
+
+                        for (var sColIndex in oFilterGroups) {
+                            var aAllowedValues = oFilterGroups[sColIndex]; 
+                            var oCell = aRow[sColIndex];
+                            
+                            if (!oCell) return false;
+
+                            var sCellVal = oCell.value !== undefined && oCell.value !== null ? oCell.value.toString().trim() : "";
+                            
+                            var bMatch = aAllowedValues.some(function(sAllowed) {
+                                return sCellVal === sAllowed; 
+                            });
+
+                            if (!bMatch) {
+                                return false; 
+                            }
+                        }
+                        return true; 
+                    }
+                });
+
+                oBinding.filter([oCustomFilter]);
             }
 
-            var oFilterGroups = {};
-            aSelectedItems.forEach(function(oItem) {
-                var aSplit = oItem.getKey().split("___");
-                var sColIndex = aSplit[0];
-                var sValue = aSplit[1];
-                
-                if (!oFilterGroups[sColIndex]) {
-                    oFilterGroups[sColIndex] = [];
-                }
-                oFilterGroups[sColIndex].push(new Filter(sColIndex + "/value", FilterOperator.EQ, sValue)); 
-            });
-
-            var aAndFilters = [];
-            for (var key in oFilterGroups) {
-                if (oFilterGroups[key].length > 1) {
-                    aAndFilters.push(new Filter({filters: oFilterGroups[key], and: false})); 
-                } else {
-                    aAndFilters.push(oFilterGroups[key][0]);
-                }
-            }
-
-            var oFinalFilter = new Filter({filters: aAndFilters, and: true}); 
-            oBinding.filter([oFinalFilter]);
+            var iFilteredLength = oBinding.getLength(); //Lấy tổng số dòng hiện có sau khi đã áp dụng filter
+            
+            var iNewVisibleRows = iFilteredLength === 0 ? 1 : (iFilteredLength < 10 ? iFilteredLength : 10);
+            
+            //Cập nhật lại trạng thái các nút
+            var bHasMore = iFilteredLength > iNewVisibleRows;
+            var bHasLess = false;
+            
+            var oDisplayModel = this.getView().getModel("displayModel");
+            oDisplayModel.setProperty("/visibleRowCount", iNewVisibleRows);
+            oDisplayModel.setProperty("/hasMore", bHasMore);
+            oDisplayModel.setProperty("/hasLess", bHasLess);
         }
     };
 });
